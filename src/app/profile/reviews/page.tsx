9e5bd8ba64be9590048a2fbb6a18fba41eda3a95
alberto.niironen <alberto.niironen@gmail.com>
2023-05-16 11:45:29 +0100

    [ADD] Reviews in new Architecture next v13

---
+++ src/app/profile/reviews/page.tsx
@@ -0,0 +1,49 @@
+import { ROUTE_SIGNIN } from "../../../config";
+import { IReview } from "../../../lib/types.d";
+import { createServerClient } from "../../../utils/supabaseServer";
+import { Reviews } from "../../../components/customLayout";
+
+export default async function ReviewsPage() {
+  const { reviews } = await getReviewsData();
+
+  return (
+    <>
+      <Reviews reviews={reviews!} />
+    </>
+  );
+}
+
+async function getReviewsData() {
+  const supabase = createServerClient();
+
+  // Check if we have a session
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session)
+    return {
+      redirect: {
+        destination: ROUTE_SIGNIN,
+        permanent: false,
+      },
+    };
+
+  const { data: reviewsData, error: reviewsError } = await supabase
+    .from("users")
+    .select(
+      `
+        *,
+        orders (*),
+        campaigns (*),
+        customize_settings (*),
+        profile_location (*)
+      `
+    )
+    .eq("id", session.user.id);
+
+  if (reviewsError) throw reviewsError;
+
+  return { reviews: reviewsData as IReview[] };
+  // }
+}
