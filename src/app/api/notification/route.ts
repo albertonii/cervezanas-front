e1c509dfa4eab4cb3a004f4b1399bf58919ee2ea
alberto.niironen <alberto.niironen@gmail.com>
2023-05-31 10:45:54 +0100

    [api-notification]

--- src/app/api/notification/route.ts
+++ src/app/api/notification/route.ts
@@ -1,9 +1,33 @@
 import { NextResponse } from "next/server";
+import { isResponseCodeOk, ResponseJSONSuccess } from "redsys-easy";
+import { processRestNotification } from "../../../components/TPV";
 import { createServerClient } from "../../../utils/supabaseServer";
 import { decodeBase64 } from "../../../utils/utils";
 
-export async function POST(request: Request) {
+export async function POST(request: Request, response: Response) {
   console.log(request);
+
+  const notificationBody = request.body as unknown as ResponseJSONSuccess;
+
+  // Always validate a notification
+  const params = processRestNotification(notificationBody);
+  const orderId = params.Ds_Order;
+
+  if (isResponseCodeOk(params.Ds_Response)) {
+    // eslint-disable-next-line no-console
+    console.log(`Payment for order ${orderId} succeded`);
+    // db.orderPayments.update(orderId, { status: "PAYMENT_SUCCEDED" });
+  } else {
+    // eslint-disable-next-line no-console
+    console.log(`Payment for order ${orderId} failed`);
+    // db.orderPayments.update(orderId, { status: "PAYMENT_FAILED" });
+  }
+
+  // In this case redsys doesn't care about the response body
+  // response.status = 204;
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+
   // const { searchParams } = new URL(request.url);
   // const { Ds_MerchantParameters } = searchParams as any;
 
