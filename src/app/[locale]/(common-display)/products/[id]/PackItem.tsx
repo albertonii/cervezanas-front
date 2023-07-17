21316184b8d527f0001cb6326982118fae959f40
alberto.niironen <alberto.niironen@gmail.com>
2023-07-17 12:53:07 +0100

    [ADD] Info in product packs display

--- src/app/[locale]/(common-display)/products/[id]/PackItem.tsx
+++ src/app/[locale]/(common-display)/products/[id]/PackItem.tsx
@@ -17,7 +17,7 @@ export default function PackItem({
   selectedPackId,
 }: Props) {
   const t = useTranslations();
-
+  console.log(pack.img_url);
   const [isHovering, setIsHovering] = useState(false);
 
   // const handleDecreaseFromCartItem = () => {
@@ -58,18 +58,29 @@ export default function PackItem({
         </label>
 
         <div
-          className={`absolute  bottom-full left-1/2 m-auto w-full -translate-x-1/2 -translate-y-[2rem] transform rounded-md bg-beer-foam shadow-xl transition-all duration-300 ease-in-out  
+          className={`absolute bottom-full left-1/2 m-auto w-[70vw] -translate-x-1/2 -translate-y-[2rem] transform rounded-md bg-beer-foam shadow-xl transition-all duration-300 ease-in-out sm:w-[35vw] lg:w-[15vw]
         ${!isHovering && "hidden"}`}
         >
-          <Image
-            src={`${SupabaseProps.BASE_PRODUCTS_URL}${decodeURIComponent(
-              pack.img_url
-            )}`}
-            width={1300}
-            height={1300}
-            alt={`Product pack - ${pack.quantity}`}
-            className={"px-2 py-2 "}
-          />
+          <div className="m-4 flex flex-col items-center text-lg">
+            <span className="font-semibold">{pack.name}</span>
+            <span className="">
+              {pack.quantity} {pack.quantity > 1 ? t("units") : t("unit")} -{" "}
+              {pack.price} €
+            </span>
+
+            <Image
+              src={`${
+                pack.img_url
+                  ? SupabaseProps.BASE_PRODUCTS_URL +
+                    decodeURIComponent(pack.img_url)
+                  : "/assets/nobeer.png"
+              }`}
+              width={1300}
+              height={1300}
+              alt={`Product pack - ${pack.quantity}`}
+              className={"w-[30vw] px-2 py-2 sm:w-[25vw] lg:w-[10vw]"}
+            />
+          </div>
         </div>
       </div>
     </li>
