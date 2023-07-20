57dddc5506c5f03b630216b0f1e7a909cc07dcf1
alberto.niironen <alberto.niironen@gmail.com>
2023-07-20 17:48:06 +0100

    [ADD] Styling for packs inside CP

--- src/app/[locale]/profile/consumption_points/AccordionItem.tsx
+++ src/app/[locale]/profile/consumption_points/AccordionItem.tsx
@@ -4,6 +4,7 @@ import { IProduct, IProductPack } from "../../../../lib/types";
 import { useAuth } from "../../../../components/Auth";
 import { Format, FormatName } from "../../../../lib/beerEnum";
 import Image from "next/image";
+import { formatCurrency } from "../../../../utils";
 
 interface Props {
   product: IProduct;
@@ -14,19 +15,17 @@ const AccordionItem: React.FC<Props> = ({ product }) => {
   const { user } = useAuth();
 
   const [showAccordion, setShowAccordion] = useState(false);
-  const [formatIcon, setFormatIcon] = useState(() => {
-    const format = product.beers[0]?.format;
-    switch (format) {
-      case FormatName.bottle:
-        return "/icons/format/bottle.svg";
-      case FormatName.can:
-        return "/icons/format/can.png";
-      case FormatName.draft:
-        return "/icons/format/keg.svg";
-      default:
-        return "/icons/format/bottle.svg";
-    }
-  });
+
+  const formatName = product.beers[0]?.format ?? "";
+  const formatIcon =
+    formatName === FormatName.bottle
+      ? "/icons/format/bottle.svg"
+      : formatName === FormatName.can
+      ? "/icons/format/can.png"
+      : formatName === FormatName.draft
+      ? "/icons/format/keg.svg"
+      : "/icons/format/bottle.svg";
+  const volume = product.beers[0]?.volume ?? "";
 
   const [selectedPacks, setSelectedPacks] = useState<{
     [productId: string]: string[];
@@ -50,60 +49,86 @@ const AccordionItem: React.FC<Props> = ({ product }) => {
   }
 
   return (
-    <div className="mb-4 rounded-lg border border-gray-200">
+    <div className="mx-4 my-2 rounded-lg border border-gray-200">
       <div
         className={`${
-          showAccordion
-            ? "bg-gray-100 text-beer-draft"
-            : "bg-white text-beer-gold"
-        } flex cursor-pointer justify-between px-6 py-4 text-lg font-semibold`}
+          showAccordion ? "bg-gray-100 text-beer-draft" : "text-beer-gold"
+        } flex cursor-pointer justify-between px-6 py-4 text-lg `}
         onClick={() => setShowAccordion(!showAccordion)}
       >
-        <span className="mr-2">{product.name}</span>
+        <span className="mr-2 font-semibold">{product.name}</span>
 
-        <Image src={formatIcon} alt="format" width={32} height={32} />
+        <div className="flex space-x-2">
+          <span className="">
+            {volume} {formatName === Format.draft.toString() ? "l" : "ml"}
+          </span>
+
+          <Image src={formatIcon} alt="format" width={32} height={32} />
+        </div>
       </div>
 
       {showAccordion && (
-        <div className="bg-white px-6 py-4">
+        <div className="px-6 py-4">
           <div className="mb-2 text-gray-500 dark:text-gray-400">
             {t("brand_header")}: [Marca del producto]
           </div>
           <div className="mb-2 text-gray-500 dark:text-gray-400">
-            {t("format_header")}: {t(product.beers[0]?.format ?? "")}
+            {t("format_header")}: {t(formatName ?? "")}
           </div>
           <div className="mb-4 text-gray-500 dark:text-gray-400">
-            {t("capacity_header")}: {t(product.beers[0]?.volume ?? "")}{" "}
-            {product.beers[0]?.format === Format.draft.toString() ? "l" : "ml"}
+            {t("capacity_header")}: {volume ?? ""}
+            {formatName === Format.draft.toString() ? "l" : "ml"}
           </div>
+
           {user && (
-            <div>
-              {product.product_packs
-                .sort((a, b) => a.quantity - b.quantity)
-                .map((pack: IProductPack) => (
-                  <div key={pack.id} className="mb-2 flex items-center">
-                    <input
-                      id={`checkbox-pack-${pack.id}`}
-                      type="checkbox"
-                      checked={selectedPacks[product.id]?.includes(pack.id)}
-                      onChange={(e) =>
-                        handleCheckboxChange(
-                          product.id,
-                          pack.id,
-                          e.target.checked
-                        )
-                      }
-                      className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-beer-blonde focus:ring-2 focus:ring-beer-blonde dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-beer-draft"
-                    />
-                    <label
-                      htmlFor={`checkbox-pack-${pack.id}`}
-                      className="ml-2 cursor-pointer text-sm font-medium text-gray-900 dark:text-gray-300"
+            <>
+              <span className="mb-4 text-lg font-semibold">
+                {" "}
+                {t("available_packs")}:
+              </span>
+
+              <div className="grid grid-cols-1 space-y-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                {product.product_packs
+                  .sort((a, b) => a.quantity - b.quantity)
+                  .map((pack: IProductPack) => (
+                    <div
+                      key={pack.id}
+                      className={`${
+                        selectedPacks[product.id]?.includes(pack.id) &&
+                        "bg-beer-softFoam"
+                      } mr-2 flex items-center rounded-lg border p-1`}
                     >
-                      {pack.name}
-                    </label>
-                  </div>
-                ))}
-            </div>
+                      <input
+                        id={`checkbox-pack-${pack.id}`}
+                        type="checkbox"
+                        checked={selectedPacks[product.id]?.includes(pack.id)}
+                        onChange={(e) =>
+                          handleCheckboxChange(
+                            product.id,
+                            pack.id,
+                            e.target.checked
+                          )
+                        }
+                        className={`h-4 w-4 rounded border-gray-300 bg-gray-100 text-beer-blonde focus:ring-2 focus:ring-beer-blonde dark:border-gray-500 dark:bg-gray-600 dark:ring-offset-gray-700 dark:focus:ring-beer-draft`}
+                      />
+                      <div>
+                        <label
+                          htmlFor={`checkbox-pack-${pack.id}`}
+                          className="ml-2 cursor-pointer text-sm font-medium text-gray-900 dark:text-gray-300"
+                        >
+                          {pack.name}
+                        </label>
+
+                        <div className="ml-2 text-xs text-gray-500 dark:text-gray-400">
+                          {pack.quantity}{" "}
+                          {pack.quantity > 1 ? t("units") : t("unit")} {" - "}
+                          {formatCurrency(pack.price)}
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </>
           )}
         </div>
       )}
