12155ed6085d2a550189dcd0fff6f07a56ca4db3
alberto.niironen <alberto.niironen@gmail.com>
2023-07-25 10:13:15 +0100

    [STYLE] Tabs

--- src/components/common/HorizontalSections.tsx
+++ src/components/common/HorizontalSections.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { ComponentProps, useState } from "react";
+import React, { ComponentProps, useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
 
 interface Props {
@@ -18,6 +18,10 @@ export default function HorizontalSections({ handleMenuClick, tabs }: Props) {
     handleMenuClick(tab);
   };
 
+  useEffect(() => {
+    console.log("activeTab", activeTab);
+  }, [activeTab]);
+
   return (
     <div className="" aria-label="">
       <ul className="flex divide-x divide-gray-200 rounded-lg pb-6 pl-72 pr-6 text-center text-sm font-medium text-gray-500 shadow dark:divide-gray-700 dark:text-gray-400">
@@ -25,14 +29,10 @@ export default function HorizontalSections({ handleMenuClick, tabs }: Props) {
           <li
             key={index}
             className={`
-          ${
-            activeTab === "account"
-              ? "bg-gray-100  text-gray-900"
-              : "bg-beer-foam"
-          }
+          ${activeTab === tab ? "bg-gray-100  text-beer-gold" : "bg-beer-foam"}
           ${index === 0 ? "rounded-l-lg" : ""}
           ${index === tabs.length - 1 ? "rounded-r-lg" : ""}
-          active flex w-full items-center justify-center p-4 hover:cursor-pointer hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:ring-4 focus:ring-beer-blonde dark:bg-gray-700 dark:text-white`}
+          active flex w-full items-center justify-center p-4 hover:cursor-pointer hover:bg-gray-50 hover:text-beer-draft focus:outline-none focus:ring-4 focus:ring-beer-blonde dark:bg-gray-700 dark:text-white`}
             onClick={() => handleClick(tab)}
           >
             {t(tab)}
