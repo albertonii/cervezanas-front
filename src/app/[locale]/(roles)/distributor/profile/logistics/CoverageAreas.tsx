3bb886fc2f26d508b6ecbd2581ff0ee2e45eab57
alberto.niironen <alberto.niironen@gmail.com>
2023-08-08 12:27:02 +0100

    [ADD] Horizontal section

--- src/app/[locale]/(roles)/distributor/profile/logistics/CoverageAreas.tsx
+++ src/app/[locale]/(roles)/distributor/profile/logistics/CoverageAreas.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { DistributionType } from "../../../../../../lib/enums";
+import HorizontalMenu from "./HorizontalMenu";
 
 export default function CoverageAreas() {
+  const [menuOption, setMenuOption] = useState<string>("products");
+
+  const renderSwitch = () => {
+    switch (menuOption) {
+      case DistributionType.LOCAL:
+        return <span>Local</span>;
+      case DistributionType.CITY:
+        return <span>Cities</span>;
+      case DistributionType.PROVINCE:
+        return <span>Provinces</span>;
+      case DistributionType.REGION:
+        return <span>Regions</span>;
+      case DistributionType.EUROPE:
+        return <span>Europe</span>;
+      case DistributionType.INTERNATIONAL:
+        return <span>international</span>;
+      default:
+        return <span>local</span>;
+    }
+  };
+
   return (
     <div>
       {/* Horizontal menu  */}
+      <HorizontalMenu setMenuOption={setMenuOption} />
 
       {/* Coverage Area content  */}
+      {renderSwitch()}
+
       {/* Map Area Content  */}
     </div>
   );
---
+++ src/lib/enums.ts
@@ -0,0 +1,8 @@
+export enum DistributionType {
+  LOCAL = "local",
+  CITY = "city",
+  PROVINCE = "province",
+  REGION = "region",
+  EUROPE = "europe",
+  INTERNATIONAL = "international",
+}
