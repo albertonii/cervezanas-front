168043c975367688f80a313416392807e6525270
alberto.niironen <alberto.niironen@gmail.com>
2023-08-17 12:59:02 +0100

    [ADD] Opt para Buttons components

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -25,25 +25,42 @@ interface ButtonProps {
 
 export function Button({
   onClick,
-  isActive,
+  isActive = false,
   children,
-  class: className,
+  class: className = "",
   box,
-  danger,
-  small,
-  medium,
-  large,
-  xLarge,
-  xxLarge,
-  disabled,
-  primary,
-  accent,
-  btnType,
-  form,
-  fullSize,
-  isLoading,
+  danger = false,
+  small = false,
+  medium = false,
+  large = false,
+  xLarge = false,
+  xxLarge = false,
+  disabled = false,
+  primary = false,
+  accent = false,
+  btnType = "",
+  form = "",
+  fullSize = false,
+  isLoading = false,
 }: ButtonProps) {
   const hoverColor = isActive ? "filled" : "unfilled";
+
+  const getSizeClass = () => {
+    if (small) return "text-md px-4 py-2";
+    if (medium) return "px-4 py-2 text-base";
+    if (large) return "px-5 py-3 text-lg";
+    if (xLarge) return "px-6 py-3 text-xl";
+    if (xxLarge) return "px-6 py-3 text-2xl";
+    return "";
+  };
+
+  const getColorClass = () => {
+    if (primary)
+      return "border-2 border-beer-blonde bg-beer-softBlonde hover:bg-beer-blonde";
+    if (accent) return "border-2 border-beer-blonde bg-beer-foam";
+    if (danger) return "bg-red-500 hover:bg-red-600";
+    return "shrink-0 hover:bg-beer-softBlonde";
+  };
 
   const getButtonType = () => {
     switch (btnType) {
@@ -63,21 +80,11 @@ export function Button({
       form={form}
       className={`
         mt-0 flex items-center justify-center rounded border-2 border-beer-blonde transition duration-100 ease-in
+        ${getSizeClass()}
+        ${getColorClass()}
         ${box ? "h-auto w-10" : ""}
         ${disabled ? "cursor-not-allowed opacity-50" : ""}
-        ${small ? "w-24" : ""} 
-        ${medium ? "w-48" : ""}
-        ${large ? "w-52" : ""}
-        ${xLarge ? "w-64" : ""}
-        ${xxLarge ? "w-80" : ""}
         ${fullSize ? "w-full" : ""}
-        ${
-          primary
-            ? " border-2 border-beer-blonde bg-beer-softBlonde hover:bg-beer-blonde"
-            : "shrink-0 hover:bg-beer-softBlonde"
-        }
-        ${accent ? "border-2 border-beer-blonde bg-beer-foam" : ""}
-        ${danger ? "bg-red-500  hover:bg-red-600 " : ""}
         ${
           isLoading &&
           "border-2 border-beer-foam bg-beer-softFoam text-beer-dark"
@@ -90,11 +97,9 @@ export function Button({
         ${danger ? "font-semibold text-beer-foam" : ""} 
         ${primary ? "font-semibold text-beer-dark" : "text-beer-dark"}}
         ${accent ? "text-beer-dark hover:text-beer-dark" : ""}
-        ${small ? "text-md px-4 py-2" : ""} 
-        ${medium ? "px-4 py-2 text-base" : ""}
-        ${large ? "px-5 py-3 text-lg" : ""}
-        ${xLarge ? "px-6 py-3 text-xl" : ""}
-        ${xxLarge ? "px-6 py-3 text-2xl" : ""}
+        ${getSizeClass()}
+
+
       `}
       >
         {children}
