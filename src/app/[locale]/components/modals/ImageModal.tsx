39dffa9a4365c235a5138d80367c77a8a7585fa5
alberto.niironen <alberto.niironen@gmail.com>
2023-09-21 15:56:20 +0100

    [FIX] Typos for database - types.d-ts

--- src/app/[locale]/components/modals/ImageModal.tsx
+++ src/app/[locale]/components/modals/ImageModal.tsx
@@ -63,9 +63,29 @@ export function ImageModal(props: Props) {
     <>
       {showModal && (
         <PortalModal wrapperId="modal-portal">
-          <div className="fixed inset-0 z-50 flex items-start justify-center overflow-y-auto overflow-x-hidden bg-beer-blonde/40 outline-none focus:outline-none">
-            hola
-          </div>
+          <>
+            <div className="fixed inset-0 z-50 flex items-start justify-center overflow-y-auto overflow-x-hidden bg-beer-blonde/40 outline-none focus:outline-none">
+              {/* The modal  */}
+              <div className="relative mx-4 my-6 h-screen w-screen shadow-lg ">
+                {/* Close modal */}
+                <div className="border-slate-200 flex items-start justify-between rounded-t border-b border-solid p-5">
+                  <button
+                    className="float-right ml-auto border-0 p-1 text-3xl font-semibold leading-none text-black outline-none focus:outline-none"
+                    onClick={() => handleClose()}
+                  >
+                    <span className=" fixed right-8 top-6 text-5xl font-bold text-beer-foam">
+                      &times;
+                    </span>
+                  </button>
+                </div>
+
+                {/*body*/}
+                <div className="mt-[5rem] grid grid-cols-1 justify-items-center">
+                  <div ref={modalRef}>{children}</div>
+                </div>
+              </div>
+            </div>
+          </>
         </PortalModal>
       )}
     </>
