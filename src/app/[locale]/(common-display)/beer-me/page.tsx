4f50920cd0272aa66fe7d8139385040c35f441e2
alberto.niironen <alberto.niironen@gmail.com>
2023-10-30 15:13:21 +0000

    [Style] Minor issue with floating dot and Beer-me not accessing

--- src/app/[locale]/(common-display)/beer-me/page.tsx
+++ src/app/[locale]/(common-display)/beer-me/page.tsx
@@ -1,6 +1,4 @@
 import Beerme from "./Beerme";
-import { redirect } from "next/navigation";
-import { VIEWS } from "../../../../constants";
 import { IConsumptionPoints } from "../../../../lib/types.d";
 import { createServerClient } from "../../../../utils/supabaseServer";
 
@@ -20,15 +18,6 @@ export default async function BeerMePage() {
 async function getCPsData() {
   // Create authenticated Supabase Client
   const supabase = createServerClient();
-
-  // Check if we have a session
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  if (!session) {
-    redirect(VIEWS.SIGN_IN);
-  }
 
   const { data: cps, error: cpsError } = await supabase
     .from("consumption_points")
--- src/app/[locale]/ScreenMenu.tsx
+++ src/app/[locale]/ScreenMenu.tsx
@@ -2,18 +2,18 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useLocale, useTranslations } from "next-intl";
 import { Select } from "@supabase/ui";
-import { useRouter } from "next/navigation";
-import { useShoppingCart } from "../../context/ShoppingCartContext";
 import { useAuth } from "./Auth/useAuth";
-import { Button } from "./components/common/Button";
-import { Notification } from "./components/Notification";
-import { useAppContext } from "../../context";
-import { ROUTE_SIGNIN } from "../../config";
 import { COMMON } from "../../constants";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { ROUTE_SIGNIN } from "../../config";
+import { useAppContext } from "../../context";
+import { Button } from "./components/common/Button";
+import { useLocale, useTranslations } from "next-intl";
+import { Notification } from "./components/Notification";
 import { HeaderDropdownButton } from "./HeaderDropdownButton";
+import { useShoppingCart } from "../../context/ShoppingCartContext";
 
 export function ScreenMenu() {
   const { user, role } = useAuth();
@@ -55,10 +55,6 @@ export function ScreenMenu() {
     router.push(`/${locale}${ROUTE_SIGNIN}`);
   };
 
-  const handleBeerMe = () => {
-    router.push(`/${locale}/beer-me`);
-  };
-
   const MENU_ITEM_STYLES =
     "block rounded py-2 pr-4 pl-3 text-sm font-semibold text-beer-dark hover:text-beer-draft dark:text-white md:bg-transparent md:p-0 lg:text-lg";
 
@@ -67,21 +63,20 @@ export function ScreenMenu() {
   };
 
   return (
-    <>
-      <div className="hidden rounded border-gray-200 dark:bg-gray-900 sm:block sm:px-4">
-        <div className="container grid grid-cols-3 bg-beer-darkGold sm:mx-auto sm:flex sm:justify-between sm:gap-2 sm:bg-transparent">
-          {/* Left elements  */}
-          <div className="flex w-full items-center justify-center ">
-            <ul className="align-center mt-4 p-4 dark:border-gray-700 dark:bg-gray-800 sm:flex md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium md:dark:bg-gray-900">
-              <li className="flex items-center">
-                <Link href="/marketplace" locale={locale}>
-                  <span className={`${MENU_ITEM_STYLES}`}>
-                    {t("marketplace").toUpperCase()}
-                  </span>
-                </Link>
-              </li>
+    <section className="hidden rounded border-gray-200 dark:bg-gray-900 sm:block sm:px-4">
+      <nav className="container grid grid-cols-3 bg-beer-darkGold sm:mx-auto sm:flex sm:justify-between sm:gap-2 sm:bg-transparent">
+        {/* Left elements  */}
+        <section className="flex w-full items-center justify-center ">
+          <ul className="align-center mt-4 p-4 dark:border-gray-700 dark:bg-gray-800 sm:flex md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium md:dark:bg-gray-900">
+            <li className="flex items-center">
+              <Link href="/marketplace" locale={locale}>
+                <span className={`${MENU_ITEM_STYLES}`}>
+                  {t("marketplace").toUpperCase()}
+                </span>
+              </Link>
+            </li>
 
-              {/* <li className="flex items-center">
+            {/* <li className="flex items-center">
                 <Link href="/community" locale={locale}>
                   <span className={`${MENU_ITEM_STYLES}`} aria-current="page">
                     {t("community").toUpperCase()}
@@ -89,193 +84,193 @@ export function ScreenMenu() {
                 </Link>
               </li> */}
 
-              <li className="flex items-center">
-                <Link href="/events" locale={locale}>
-                  <span className={`${MENU_ITEM_STYLES}`} aria-current="page">
-                    {t("events").toUpperCase()}
-                  </span>
-                </Link>
-              </li>
-            </ul>
-          </div>
+            <li className="flex items-center">
+              <Link href="/events" locale={locale}>
+                <span className={`${MENU_ITEM_STYLES}`} aria-current="page">
+                  {t("events").toUpperCase()}
+                </span>
+              </Link>
+            </li>
+          </ul>
+        </section>
 
-          {/* Logo Cervezanas  */}
-          <div className="w-full" id="navbar-default">
-            <div className="relative flex h-16 w-full flex-shrink-0 justify-center md:h-20 lg:h-24">
-              <div className="relative flex h-[100px] w-[110px] justify-center bg-beer-gold p-2 sm:h-[143px] sm:w-[141px] sm:p-2 lg:h-[153] lg:w-[151px] ">
-                <Link href={"/"} locale={locale}>
-                  <Image
-                    src="/logo_cervezanas.svg"
-                    alt="Cervezanas Logo"
-                    width={160}
-                    height={160}
-                    style={{ objectFit: "contain" }}
-                    priority={true}
-                    sizes="100px"
-                  />
-                </Link>
-                <div className="absolute -bottom-5 h-[22px] w-full bg-beer-darkGold pt-[22px]"></div>
-              </div>
+        {/* Logo Cervezanas  */}
+        <section className="w-full" id="navbar-default">
+          <div className="relative flex h-16 w-full flex-shrink-0 justify-center md:h-20 lg:h-24">
+            <div className="relative flex h-[100px] w-[110px] justify-center bg-beer-gold p-2 sm:h-[143px] sm:w-[141px] sm:p-2 lg:h-[153] lg:w-[151px] ">
+              <Link href={"/"} locale={locale}>
+                <Image
+                  src="/logo_cervezanas.svg"
+                  alt="Cervezanas Logo"
+                  width={160}
+                  height={160}
+                  style={{ objectFit: "contain" }}
+                  priority={true}
+                  sizes="100px"
+                />
+              </Link>
+              <p className="absolute -bottom-5 h-[22px] w-full bg-beer-darkGold pt-[22px]"></p>
             </div>
           </div>
+        </section>
 
-          {/* Right elements  */}
-          <div className="w-full ">
-            <ul className="py-2 dark:border-gray-700 dark:bg-gray-800 sm:mt-4 sm:flex sm:flex-row sm:justify-end sm:p-4 sm:align-middle md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium md:dark:bg-gray-900">
-              {/* Language  */}
-              <li className="flex max-w-[50px] items-center">
-                <Select
-                  size="tiny"
-                  name="language"
-                  style={{
-                    backgroundColor: "transparent",
-                    maxWidth: "50px",
-                  }}
-                  onChange={onChangeLanguage}
-                  className=""
-                >
-                  <Select.Option value="es">🇪🇸</Select.Option>
-                  <Select.Option value="en">🇬🇧</Select.Option>
-                </Select>
-              </li>
+        {/* Right elements  */}
+        <section className="w-full ">
+          <ul className="py-2 dark:border-gray-700 dark:bg-gray-800 sm:mt-4 sm:flex sm:flex-row sm:justify-end sm:p-4 sm:align-middle md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium md:dark:bg-gray-900">
+            {/* Language  */}
+            <li className="flex max-w-[50px] items-center">
+              <Select
+                size="tiny"
+                name="language"
+                style={{
+                  backgroundColor: "transparent",
+                  maxWidth: "50px",
+                }}
+                onChange={onChangeLanguage}
+                className=""
+              >
+                <Select.Option value="es">🇪🇸</Select.Option>
+                <Select.Option value="en">🇬🇧</Select.Option>
+              </Select>
+            </li>
 
-              {!user ? (
-                <>
-                  <li className="flex items-center">
-                    <Button onClick={() => handleSignIn()} title={""}>
-                      <div className="mx-2 my-1 flex items-center justify-center space-x-2">
-                        <Image
-                          src={COMMON.PROFILE_IMG}
-                          width={25}
-                          height={25}
-                          alt={"Login"}
-                        />
-                        <span>{t("my_account")}</span>
-                      </div>
-                    </Button>
-                  </li>
-                </>
-              ) : (
-                <>
-                  {/* Cart  */}
-                  {role !== "admin" && (
-                    <li
-                      className={`items´center flex ${
-                        animateShoppingCart && "animate-wiggle"
-                      }`}
-                    >
-                      <Button
-                        class={
-                          "border-none transition-all hover:scale-110 hover:cursor-pointer hover:bg-transparent"
-                        }
-                        onClick={() => openCart()}
-                        title={""}
-                      >
-                        <div className="relative rounded-full">
-                          <Image
-                            src={"/icons/shopping-cart.svg"}
-                            width={45}
-                            height={45}
-                            alt={"Go to Shopping cart"}
-                            className={"rounded-full"}
-                          />
-                          <div
-                            className={`white absolute bottom-0 right-0 flex h-6 w-6 translate-x-2 translate-y-2 items-center justify-center rounded-full bg-beer-blonde 
-                          `}
-                          >
-                            {cartQuantity}
-                          </div>
-                        </div>
-                      </Button>
-                    </li>
-                  )}
-
-                  {/* Notifications  */}
+            {!user ? (
+              <>
+                <li className="flex items-center">
+                  <Button onClick={() => handleSignIn()} title={""}>
+                    <section className="mx-2 my-1 flex items-center justify-center space-x-2">
+                      <Image
+                        src={COMMON.PROFILE_IMG}
+                        width={25}
+                        height={25}
+                        alt={"Login"}
+                      />
+                      <span>{t("my_account")}</span>
+                    </section>
+                  </Button>
+                </li>
+              </>
+            ) : (
+              <>
+                {/* Cart  */}
+                {role !== "admin" && (
                   <li
-                    className={`relative flex items-center ${
-                      animateNotifications && "animate-wiggle"
+                    className={`items´center flex ${
+                      animateShoppingCart && "animate-wiggle"
                     }`}
                   >
                     <Button
                       class={
                         "border-none transition-all hover:scale-110 hover:cursor-pointer hover:bg-transparent"
                       }
-                      onClick={() => handleClickBell()}
+                      onClick={() => openCart()}
                       title={""}
                     >
-                      <div className="relative rounded-full">
+                      <section className="relative rounded-full">
                         <Image
-                          src={"/icons/notification-icon.svg"}
+                          src={"/icons/shopping-cart.svg"}
                           width={45}
                           height={45}
-                          alt={"Notification bell"}
+                          alt={"Go to Shopping cart"}
                           className={"rounded-full"}
                         />
-                        <div className="white absolute bottom-0 right-0 flex h-6 w-6 translate-x-2 translate-y-2 items-center justify-center rounded-full bg-beer-blonde">
-                          {notifications?.length ?? 0}
+                        <div
+                          className={`white absolute bottom-0 right-0 flex h-6 w-6 translate-x-2 translate-y-2 items-center justify-center rounded-full bg-beer-blonde 
+                          `}
+                        >
+                          {cartQuantity}
                         </div>
-                      </div>
+                      </section>
                     </Button>
-
-                    {/* Notification popup  */}
-                    <Notification
-                      open={openNotification}
-                      setOpen={setOpenNotification}
-                    />
                   </li>
+                )}
 
-                  <li className="flex items-center">
-                    <HeaderDropdownButton
-                      options={
-                        role === "admin"
-                          ? ["submitted_aps", "monthly_products", "signout"]
-                          : role === "distributor"
-                          ? ["profile", "logistics", "signout"]
-                          : role === "producer"
-                          ? [
-                              "profile",
-                              "online_orders",
-                              "event_orders",
-                              "campaigns",
-                              "signout",
-                            ]
-                          : [
-                              "profile",
-                              "online_orders",
-                              "event_orders",
-                              "signout",
-                            ]
-                      }
-                    />
-                  </li>
-                </>
-              )}
-            </ul>
+                {/* Notifications  */}
+                <li
+                  className={`relative flex items-center ${
+                    animateNotifications && "animate-wiggle"
+                  }`}
+                >
+                  <Button
+                    class={
+                      "border-none transition-all hover:scale-110 hover:cursor-pointer hover:bg-transparent"
+                    }
+                    onClick={() => handleClickBell()}
+                    title={""}
+                  >
+                    <section className="relative rounded-full">
+                      <Image
+                        src={"/icons/notification-icon.svg"}
+                        width={45}
+                        height={45}
+                        alt={"Notification bell"}
+                        className={"rounded-full"}
+                      />
+                      <div className="white absolute bottom-0 right-0 flex h-6 w-6 translate-x-2 translate-y-2 items-center justify-center rounded-full bg-beer-blonde">
+                        {notifications?.length ?? 0}
+                      </div>
+                    </section>
+                  </Button>
 
-            <Button
-              onClick={() => handleBeerMe()}
-              title={"Find Cervezanas spots"}
+                  {/* Notification popup  */}
+                  <Notification
+                    open={openNotification}
+                    setOpen={setOpenNotification}
+                  />
+                </li>
+
+                <li className="flex items-center">
+                  <HeaderDropdownButton
+                    options={
+                      role === "admin"
+                        ? ["submitted_aps", "monthly_products", "signout"]
+                        : role === "distributor"
+                        ? ["profile", "logistics", "signout"]
+                        : role === "producer"
+                        ? [
+                            "profile",
+                            "online_orders",
+                            "event_orders",
+                            "campaigns",
+                            "signout",
+                          ]
+                        : [
+                            "profile",
+                            "online_orders",
+                            "event_orders",
+                            "signout",
+                          ]
+                    }
+                  />
+                </li>
+              </>
+            )}
+          </ul>
+
+          <section className="absolute right-0 top-10 rounded-l-lg bg-beer-dark sm:top-24">
+            <Link
+              href={"/beer-me"}
+              locale={locale}
+              className="flex flex-col items-center justify-start space-x-4 sm:flex-row sm:rounded-l sm:px-2 sm:py-1"
             >
-              <div className="absolute right-0 top-10 flex flex-col items-center justify-start space-x-4 rounded-l-lg bg-beer-dark sm:top-24 sm:flex-row sm:rounded-l sm:px-2 sm:py-1">
-                <Image
-                  src={"/icons/beerme.svg"}
-                  width={45}
-                  height={45}
-                  alt={"Find Cervezanas spots"}
-                  className={
-                    "mx-4 my-2 w-10 rounded-full sm:mx-0 sm:my-0 sm:w-12"
-                  }
-                />
+              <Image
+                src={"/icons/beerme.svg"}
+                width={45}
+                height={45}
+                alt={"Find Cervezanas spots"}
+                className={
+                  "mx-4 my-2 w-10 rounded-full sm:mx-0 sm:my-0 sm:w-12"
+                }
+              />
 
-                <div className="sm:flex sm:flex-col">
-                  <div className="text-right text-beer-foam ">Puntos</div>
-                  <div className="text-right text-beer-foam ">Cervezanas</div>
-                </div>
+              <div className="sm:flex sm:flex-col">
+                <span className="text-right text-beer-foam ">Puntos</span>
+                <span className="text-right text-beer-foam ">Cervezanas</span>
               </div>
-            </Button>
-          </div>
-        </div>
-      </div>
-    </>
+            </Link>
+          </section>
+        </section>
+      </nav>
+    </section>
   );
 }
