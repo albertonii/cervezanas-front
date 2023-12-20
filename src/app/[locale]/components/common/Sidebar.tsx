2a36056e04ba871021683e347de3c6c3a227a699
alberto.niironen <alberto.niironen@gmail.com>
2023-12-20 12:21:09 +0000

    [FIX] minor issue with css

--- src/app/[locale]/components/common/Sidebar.tsx
+++ src/app/[locale]/components/common/Sidebar.tsx
@@ -99,34 +99,6 @@ export function Sidebar({ sidebarLinks }: Props) {
         </>
       )}
 
-      {device === "Desktop" && (
-        <Button
-          data-drawer-target="default-sidebar"
-          data-drawer-toggle="default-sidebar"
-          aria-controls="default-sidebar"
-          btnType="button"
-          class={`mx-2 mt-2 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600`}
-          onClick={() => {
-            handleClick();
-          }}
-        >
-          <span className="sr-only">Open sidebar</span>
-          <svg
-            className="h-6 w-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              clipRule="evenodd"
-              fillRule="evenodd"
-              d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-            ></path>
-          </svg>
-        </Button>
-      )}
-
       {role && (
         <aside
           className={`
