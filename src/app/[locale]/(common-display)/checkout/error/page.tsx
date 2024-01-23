b3c040f80f326a4ff02f6a9cec4459635004a5a6
alberto.niironen <alberto.niironen@gmail.com>
2024-01-23 11:33:15 +0000

    [FIX] Typos

--- src/app/[locale]/(common-display)/checkout/error/page.tsx
+++ src/app/[locale]/(common-display)/checkout/error/page.tsx
@@ -1,11 +1,11 @@
 import ErrorCheckout from "./ErrorCheckout";
 import React from "react";
 import { redirect } from "next/navigation";
-import { VIEWS } from "../../../../../../../../constants";
-import { IOrder } from "../../../../../../../../lib/types";
-import { decodeBase64 } from "../../../../../../../../utils/utils";
-import createServerClient from "../../../../../../../../utils/supabaseServer";
-import readUserSession from "../../../../../../../../lib/actions";
+import { decodeBase64 } from "../../../../../utils/utils";
+import createServerClient from "../../../../../utils/supabaseServer";
+import readUserSession from "../../../../../lib/actions";
+import { VIEWS } from "../../../../../constants";
+import { IOrder } from "../../../../../lib/types";
 
 export async function generateMetadata({ searchParams }: any) {
   try {
