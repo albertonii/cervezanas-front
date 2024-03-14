f6f450e12fe65d9d628c226f85a87eea20acd97f
alberto.niironen <alberto.niironen@gmail.com>
2024-03-14 10:35:14 +0000

    [FIX] Error fetch experiences list

--- src/app/[locale]/(roles)/producer/profile/experiences/page.tsx
+++ src/app/[locale]/(roles)/producer/profile/experiences/page.tsx
@@ -7,68 +7,70 @@ import { VIEWS } from '../../../../../../constants';
 import { IExperience } from '../../../../../../lib/types/quiz';
 
 export default async function EventsPage() {
-  const experiencesData = getExperiencesData();
-  const experiencesCounterData = getExperiencesCounter();
-  const [experiences, experiencesCounter] = await Promise.all([
-    experiencesData,
-    experiencesCounterData,
-  ]);
+    const experiencesData = getExperiencesData();
+    const experiencesCounterData = getExperiencesCounter();
+    const [experiences, experiencesCounter] = await Promise.all([
+        experiencesData,
+        experiencesCounterData,
+    ]);
 
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Experiences experiences={experiences} counter={experiencesCounter} />
-    </Suspense>
-  );
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <Experiences
+                experiences={experiences}
+                counter={experiencesCounter}
+            />
+        </Suspense>
+    );
 }
 
 async function getExperiencesData() {
-  const supabase = await createServerClient();
+    const supabase = await createServerClient();
 
-  const session = await readUserSession();
+    const session = await readUserSession();
 
-  if (!session) {
-    redirect(VIEWS.SIGN_IN);
-  }
+    if (!session) {
+        redirect(VIEWS.SIGN_IN);
+    }
 
-  const { data, error } = await supabase
-    .from('experiences')
-    .select(
-      `
-        *,
-        bm_questions (
-          id,
-          question,
-          experience_id,
-          product_id,
-          correct_answer,
-          incorrect_answers,
-          difficulty,
-          type
+    const { data, error } = await supabase
+        .from('experiences')
+        .select(
+            `
+          *,
+          bm_questions (
+            id,
+            question,
+            experience_id,
+            product_id,
+            correct_answer,
+            incorrect_answers,
+            difficulty,
+            type
+          )
+          `,
         )
-      `,
-    )
-    .eq('producer_id', session.id);
+        .eq('producer_id', session.id);
 
-  if (error) throw error;
-
-  return data as IExperience[];
+    if (error) throw error;
+    return data as IExperience[];
 }
 
 async function getExperiencesCounter() {
-  const supabase = await createServerClient();
+    const supabase = await createServerClient();
 
-  const session = await readUserSession();
+    const session = await readUserSession();
 
-  if (!session) {
-    redirect(VIEWS.SIGN_IN);
-  }
+    if (!session) {
+        redirect(VIEWS.SIGN_IN);
+    }
 
-  const { count, error } = await supabase
-    .from('experiences')
-    .select('id', { count: 'exact' }) // Selecciona solo una columna y habilita el conteo
-    .eq('producer_id', session.id);
+    const { count, error } = await supabase
+        .from('experiences')
+        .select('id', { count: 'exact' }) // Selecciona solo una columna y habilita el conteo
+        .eq('producer_id', session.id);
 
-  if (error) throw error;
+    if (error) throw error;
 
-  return count as number | 0;
+    return count as number | 0;
 }
