75a2082f83a3b3530da5ef6f203658a1005fdfa7
alberto.niironen <alberto.niironen@gmail.com>
2024-03-14 13:31:27 +0000

    [FIX] Cannot read id event cart in EVENT ID

--- src/app/[locale]/(common-display)/(events-community)/events/[id]/(cart)/EventCart.tsx
+++ src/app/[locale]/(common-display)/(events-community)/events/[id]/(cart)/EventCart.tsx
@@ -1,44 +1,47 @@
 import MinimizedCart from './MinimizedCart';
 import MaxifiedCart from './MaxifiedCart';
+import useEventCartStore from '../../../../../../store/eventCartStore';
 import React, { useEffect, useState } from 'react';
 import { IProductPackEventCartItem } from '../../../../../../../lib/types/types';
-import useEventCartStore from '../../../../../../store/eventCartStore';
 
 interface Props {
-  eventId: string;
+    eventId: string;
 }
 
 export default function EventCart({ eventId }: Props) {
-  const [isMinimized, setIsMinimized] = React.useState<boolean>(false);
+    const [isMinimized, setIsMinimized] = React.useState<boolean>(false);
 
-  const { eventCarts, existEventCart, createNewCart } = useEventCartStore();
+    const { eventCarts, existEventCart, createNewCart } = useEventCartStore();
 
-  const [items, setItems] = useState<IProductPackEventCartItem[]>([]);
+    const [items, setItems] = useState<IProductPackEventCartItem[]>([]);
 
-  useEffect(() => {
-    if (!existEventCart(eventId)) {
-      createNewCart(eventId);
-    }
+    useEffect(() => {
+        if (!existEventCart(eventId)) {
+            createNewCart(eventId);
+        }
 
-    setItems(eventCarts[eventId]);
-  }, [eventCarts]);
+        setItems(eventCarts[eventId]);
+    }, [eventCarts]);
 
-  return (
-    <section
-      className={`fixed -top-20 left-10  z-40 rounded-lg border-2 border-beer-softBlonde bg-beer-softFoam px-2 py-2 shadow-md sm:left-0 sm:right-auto sm:w-auto md:left-10`}
-      aria-modal="true"
-      role="dialog"
-      tabIndex={-1}
-    >
-      {isMinimized ? (
-        <MinimizedCart eventId={eventId} setIsMinimized={setIsMinimized} />
-      ) : (
-        <MaxifiedCart
-          setIsMinimized={setIsMinimized}
-          items={items}
-          eventId={eventId}
-        />
-      )}
-    </section>
-  );
+    return (
+        <section
+            className={`fixed -top-20 left-10  z-40 rounded-lg border-2 border-beer-softBlonde bg-beer-softFoam px-2 py-2 shadow-md sm:left-0 sm:right-auto sm:w-auto md:left-10`}
+            aria-modal="true"
+            role="dialog"
+            tabIndex={-1}
+        >
+            {isMinimized ? (
+                <MinimizedCart
+                    eventId={eventId}
+                    setIsMinimized={setIsMinimized}
+                />
+            ) : (
+                <MaxifiedCart
+                    setIsMinimized={setIsMinimized}
+                    items={items}
+                    eventId={eventId}
+                />
+            )}
+        </section>
+    );
 }
