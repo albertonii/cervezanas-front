70a242d4bbb533da689ec293b0e621300344fe1e
alberto.niironen <alberto.niironen@gmail.com>
2024-03-15 11:56:45 +0000

    minor

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -11,87 +11,88 @@ import { INotification } from '../../lib/types/types';
 import { MessageList } from './components/message/MessageList';
 
 type LayoutProps = {
-  children: React.ReactNode;
-  params: {
-    locale: string;
-  };
+    children: React.ReactNode;
+    params: {
+        locale: string;
+    };
 };
 
 // This will ensure that every time a new route is loaded, our session data in RootLayout will always be up-to-date.
 export const revalidate = 0;
 
 export default async function AppLocaleLayout({
-  children,
-  params: { locale },
+    children,
+    params: { locale },
 }: LayoutProps) {
-  const supabase = await createServerClient();
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+    const supabase = await createServerClient();
+    const {
+        data: { session },
+    } = await supabase.auth.getSession();
 
-  let messages;
-  try {
-    messages = (await import(`../../lib/translations/messages/${locale}.json`))
-      .default;
-  } catch (error) {
-    notFound();
-  }
+    let messages;
+    try {
+        messages = (
+            await import(`../../lib/translations/messages/${locale}.json`)
+        ).default;
+    } catch (error) {
+        notFound();
+    }
 
-  const notifications = await getNotifications();
+    const notifications = await getNotifications();
 
-  const i18n = {
-    defaultLocale: 'es',
-    locales: ['es', 'en'],
-  };
+    const i18n = {
+        defaultLocale: 'es',
+        locales: ['es', 'en'],
+    };
 
-  return (
-    <Providers session={session} messages={messages} locale={locale}>
-      <section className="relative flex flex-col bg-beer-foam">
-        <Header
-          notifications={notifications ?? []}
-          i18nLocaleArray={i18n.locales}
-        />
-        <section
-          className={classNames(
-            'relative mx-auto min-h-0 w-full overflow-auto',
-            // "h-[calc(100vh - 340px)] mx-auto mt-[10vh] w-full overflow-y-auto"
-          )}
-        >
-          {/* <Breadcrumb getDefaultTextGenerator={(path) => titleize(path)} /> */}
-        </section>
+    return (
+        <Providers session={session} messages={messages} locale={locale}>
+            <section className="relative flex flex-col bg-beer-foam">
+                <Header
+                    notifications={notifications ?? []}
+                    i18nLocaleArray={i18n.locales}
+                />
+                <section
+                    className={classNames(
+                        'relative mx-auto min-h-0 w-full overflow-auto',
+                        // "h-[calc(100vh - 340px)] mx-auto mt-[10vh] w-full overflow-y-auto"
+                    )}
+                >
+                    {/* <Breadcrumb getDefaultTextGenerator={(path) => titleize(path)} /> */}
+                </section>
 
-        <main
-          className={classNames(
-            'relative mx-auto min-h-screen w-full transform pt-0 transition lg:container',
-          )}
-        >
-          <MessageList />
-          {children}
-        </main>
-        <Footer />
-      </section>
-    </Providers>
-  );
+                <main
+                    className={classNames(
+                        'relative mx-auto min-h-screen w-full transform pt-0 transition lg:container',
+                    )}
+                >
+                    <MessageList />
+                    {children}
+                </main>
+                <Footer />
+            </section>
+        </Providers>
+    );
 }
 
 const getNotifications = async () => {
-  const supabase = await createServerClient();
+    const supabase = await createServerClient();
 
-  // Be careful when protecting pages. The server gets the user session from the cookies, which can be spoofed by anyone.
-  const session = await readUserSession();
+    // Be careful when protecting pages. The server gets the user session from the cookies, which can be spoofed by anyone.
+    const session = await readUserSession();
 
-  if (!session) return;
+    if (!session) return;
 
-  const { data: notifications, error: notificationsError } = await supabase
-    .from('notifications')
-    .select(
-      `
+    const { data: notifications, error: notificationsError } = await supabase
+        .from('notifications')
+        .select(
+            `
       *
     `,
-    )
-    .eq('read', false)
-    .eq('user_id', session.id);
+        )
+        .eq('read', false)
+        .eq('user_id', session.id);
 
-  if (notificationsError) throw notificationsError;
-  return notifications as INotification[];
+    if (notificationsError) throw notificationsError;
+    return notifications as INotification[];
 };
