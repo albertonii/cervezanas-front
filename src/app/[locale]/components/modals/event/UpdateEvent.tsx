0705287f5fa95950f2ff3f399235191a2a0ce427
alberto.niironen <alberto.niironen@gmail.com>
2024-03-15 18:58:14 +0000

    fix

--- src/app/[locale]/components/modals/event/UpdateEvent.tsx
+++ src/app/[locale]/components/modals/event/UpdateEvent.tsx
@@ -249,14 +249,15 @@ export default function UpdateEventModal({
                                     className="relative inline-flex cursor-pointer items-center"
                                     htmlFor="is_activated"
                                 >
-                                    <input
+                                    {/* TODO: INFORMAR SI ESTÁ ACTIVADO O NO */}
+                                    {/* <input
                                         id="is_activated"
                                         type="checkbox"
                                         className="peer sr-only"
                                         {...register('is_activated', {
                                             required: true,
                                         })}
-                                    />
+                                    /> */}
 
                                     <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-beer-blonde peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-beer-softFoam dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-beer-blonde"></div>
 
