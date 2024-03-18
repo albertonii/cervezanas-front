6405f804ab27b6f8f4380d0b307109eb39ca239c
alberto.niironen <alberto.niironen@gmail.com>
2024-03-18 11:14:01 +0000

    [ADD] Error marketplace tracking

--- src/app/[locale]/(common-display)/marketplace/Marketplace.tsx
+++ src/app/[locale]/(common-display)/marketplace/Marketplace.tsx
@@ -16,24 +16,24 @@ export default function Marketplace({ products }: Props) {
     const filteredProducts = filterProducts(products);
     return (
         <section className="container mx-auto bg-cerv-coal bg-[url('/assets/rec-graf6.png')] bg-contain sm:py-2 lg:py-3 ">
-            {/* <MarketplaceHeader>
-        <Filters />
-      </MarketplaceHeader> */}
+            <MarketplaceHeader>
+                <Filters />
+            </MarketplaceHeader>
 
-            {/* <article className="m-auto grid grid-cols-1 bg-white pt-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
+            <article className="m-auto grid grid-cols-1 bg-white pt-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
                 {filteredProducts &&
                     filteredProducts.map((product) => (
                         <div
                             key={product.id}
                             className="container mb-6 h-full px-3"
                         >
-                            <StoreItem
+                            {/* <StoreItem
                                 products={filteredProducts}
                                 product={product}
-                            />
+                            /> */}
                         </div>
                     ))}
-            </article> */}
+            </article>
         </section>
     );
 }
