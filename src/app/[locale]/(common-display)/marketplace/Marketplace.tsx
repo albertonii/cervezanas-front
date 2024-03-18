b73cd2c07729d706dd8e3543e30bd46bac687ede
alberto.niironen <alberto.niironen@gmail.com>
2024-03-18 11:08:11 +0000

    [ADD] Error marketplace tracking

--- src/app/[locale]/(common-display)/marketplace/Marketplace.tsx
+++ src/app/[locale]/(common-display)/marketplace/Marketplace.tsx
@@ -8,26 +8,32 @@ import { Filters } from '../../components/Filters';
 import { MarketplaceHeader } from '../../components/MarketplaceHeader';
 
 interface Props {
-  products: IProduct[];
+    products: IProduct[];
 }
 
 export default function Marketplace({ products }: Props) {
-  const { filterProducts } = useFilters();
-  const filteredProducts = filterProducts(products);
-  return (
-    <section className="container mx-auto bg-cerv-coal bg-[url('/assets/rec-graf6.png')] bg-contain sm:py-2 lg:py-3 ">
-      <MarketplaceHeader>
+    const { filterProducts } = useFilters();
+    const filteredProducts = filterProducts(products);
+    return (
+        <section className="container mx-auto bg-cerv-coal bg-[url('/assets/rec-graf6.png')] bg-contain sm:py-2 lg:py-3 ">
+            {/* <MarketplaceHeader>
         <Filters />
-      </MarketplaceHeader>
+      </MarketplaceHeader> */}
 
-      <article className="m-auto grid grid-cols-1 bg-white pt-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
-        {filteredProducts &&
-          filteredProducts.map((product) => (
-            <div key={product.id} className="container mb-6 h-full px-3">
-              <StoreItem products={filteredProducts} product={product} />
-            </div>
-          ))}
-      </article>
-    </section>
-  );
+            <article className="m-auto grid grid-cols-1 bg-white pt-10 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
+                {filteredProducts &&
+                    filteredProducts.map((product) => (
+                        <div
+                            key={product.id}
+                            className="container mb-6 h-full px-3"
+                        >
+                            <StoreItem
+                                products={filteredProducts}
+                                product={product}
+                            />
+                        </div>
+                    ))}
+            </article>
+        </section>
+    );
 }
