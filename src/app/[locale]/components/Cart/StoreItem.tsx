d594c216b96f47974c5e0bcf61876b60adc5d62c
alberto.niironen <alberto.niironen@gmail.com>
2024-03-18 11:42:46 +0000

    [ADD] Error marketplace tracking

--- src/app/[locale]/components/Cart/StoreItem.tsx
+++ src/app/[locale]/components/Cart/StoreItem.tsx
@@ -45,11 +45,14 @@ export function StoreItem({ product }: StoreItemProps) {
 
     useEffect(() => {
         if (packs) {
-            const lowestPack = packs.sort((a, b) => {
-                console.log(a);
-                console.log(b);
-                return a.quantity - b.quantity;
-            })[0] as IProductPack;
+            if (packs.length === 1) {
+                setSelectedPack(packs[0]);
+                return;
+            }
+
+            const lowestPack = packs.sort(
+                (a, b) => a.quantity - b.quantity,
+            )[0] as IProductPack;
             setSelectedPack(lowestPack);
             // setSelectedPack(packs[0]);
         }
