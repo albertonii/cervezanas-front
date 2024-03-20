9ece1099f377fbf9d2c514324e6d88df04fa5000
alberto.niironen <alberto.niironen@gmail.com>
2024-03-20 13:23:10 +0000

    [ADD] styling

--- src/app/[locale]/(roles)/producer/profile/consumption_points/ListCPFixed.tsx
+++ src/app/[locale]/(roles)/producer/profile/consumption_points/ListCPFixed.tsx
@@ -152,7 +152,7 @@ export function ListCPFixed({ cpsId }: Props) {
                     />
 
                     <div className="overflow-x-scroll border-2 ">
-                        <table className="text-center text-sm text-gray-500 dark:text-gray-400 border-2 ">
+                        <table className="w-full text-center text-sm text-gray-500 dark:text-gray-400 border-2 ">
                             <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
                                 <tr className="w-full">
                                     <th
