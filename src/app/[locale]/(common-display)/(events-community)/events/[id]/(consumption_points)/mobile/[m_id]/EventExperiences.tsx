52c1079b2a1d84c4e2b356b53887fbdad234bec2
alberto.niironen <alberto.niironen@gmail.com>
2024-03-23 11:54:43 +0000

    [some ux]

--- src/app/[locale]/(common-display)/(events-community)/events/[id]/(consumption_points)/mobile/[m_id]/EventExperiences.tsx
+++ src/app/[locale]/(common-display)/(events-community)/events/[id]/(consumption_points)/mobile/[m_id]/EventExperiences.tsx
@@ -3,15 +3,19 @@ import { IEventExperience } from '../../../../../../../../../lib/types/types';
 import EventExperienceDetails from './EventExperienceDetails';
 
 interface Props {
-  eventExperiences: IEventExperience[];
+    eventExperiences: IEventExperience[];
 }
 
 export default function EventExperiences({ eventExperiences }: Props) {
-  return (
-    <section className="w-fit sm:max-w-lg max-w-md">
-      {eventExperiences.map((experience) => {
-        return <EventExperienceDetails eventExperience={experience} />;
-      })}
-    </section>
-  );
+    return (
+        <section className="w-fit grid-cols-1 sm:w-full grid sm:grid-cols-2 gap-2">
+            {eventExperiences.map((experience) => {
+                return (
+                    <div className="col-span-1">
+                        <EventExperienceDetails eventExperience={experience} />
+                    </div>
+                );
+            })}
+        </section>
+    );
 }
