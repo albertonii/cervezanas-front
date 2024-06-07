3c034077c33135181e6772aa355bcf5e13fb2376
alberto.niironen <alberto.niironen@gmail.com>
2024-06-07 11:42:35 +0100

    [ADD] Stepper Buttons

--- src/app/[locale]/components/products/ProductStepper.tsx
+++ src/app/[locale]/components/products/ProductStepper.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslations } from 'next-intl';
+import ProductStepperButtons from './ProductStepperButtons';
 
 interface Props {
     activeStep: number;
@@ -213,58 +214,18 @@ export function ProductStepper({
                 </figure>
             </div>
 
-            <div className="mt-8 ">
+            <div className="mt-8 pt-8">
+                <ProductStepperButtons
+                    activeStep={activeStep}
+                    handleStepper={handleStepper}
+                />
+
                 <div>{children}</div>
 
-                <div className="mt-4 flex p-2">
-                    {activeStep >= 1 && (
-                        <button
-                            type="button"
-                            disabled={activeStep === 0}
-                            onClick={() => handleStepper(activeStep - 1)}
-                            className="flex cursor-pointer justify-center rounded border border-gray-600 bg-gray-100 px-4 py-2 text-base 
-                            font-bold  
-                            text-gray-700 
-                            transition 
-                            duration-200 ease-in-out hover:scale-110 
-                            hover:bg-gray-200 focus:outline-none"
-                        >
-                            {t('back')}
-                        </button>
-                    )}
-
-                    <div className="flex flex-auto flex-row-reverse">
-                        {activeStep < 3 && (
-                            <button
-                                type="button"
-                                onClick={() => handleStepper(activeStep + 1)}
-                                className="ml-2  flex  cursor-pointer justify-center rounded border border-beer-draft bg-beer-blonde px-4 py-2 text-base 
-                                    font-bold  
-                                    text-beer-draft 
-                                    transition 
-                                    duration-200 ease-in-out hover:scale-110 
-                                    hover:bg-beer-softBlondeBubble focus:outline-none"
-                            >
-                                {t('next')}
-                            </button>
-                        )}
-
-                        {/* {activeStep !== 3 && (
-                            <button
-                                type="button"
-                                onClick={() => handleStepper(3)}
-                                className="flex cursor-pointer justify-center rounded border border-beer-draft bg-bear-alvine px-4 py-2 text-base 
-                                    font-bold  
-                                    text-beer-draft 
-                                    transition 
-                                    duration-200 ease-in-out hover:scale-110 
-                                    hover:bg-bear-blonde focus:outline-none"
-                            >
-                                {t('skip')}
-                            </button>
-                        )} */}
-                    </div>
-                </div>
+                <ProductStepperButtons
+                    activeStep={activeStep}
+                    handleStepper={handleStepper}
+                />
             </div>
         </section>
     );
